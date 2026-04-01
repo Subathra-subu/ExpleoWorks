20e331920f16ae386626facc4b6291b6f76bdc49
Subathra-subu <subathrasubu2004@gmail.com>
2026-04-01 11:59:09 +0530

    Example file updated

--- Selenium/SeleniumBasics/src/main/java/demo/Example.java
+++ Selenium/SeleniumBasics/src/main/java/demo/Example.java
@@ -3,7 +3,7 @@ package demo;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class BrowserCommands {
+public class Example {
 
 	public static void main(String[] args) {
 		
