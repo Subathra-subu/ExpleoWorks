afbc02bfdd9e5de781c955f5e6ee42568fb78f75
Subathra-subu <subathrasubu2004@gmail.com>
2026-04-08 21:41:12 +0530

    Table handling programs updated

---
+++ Selenium/SeleniumBasics/src/main/java/actionClass/AlertSimple.java
@@ -0,0 +1,22 @@
+package actionClass;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class AlertSimple {
+
+	public static void main(String[] args) {
+		
+			WebDriver driver = new ChromeDriver();
+	        driver.manage().window().maximize();
+	        driver.get("http://omayo.blogspot.com/#");
+	        
+	        driver.findElement(By.id("alert1")).click();
+	        Alert alert=driver.switchTo().alert();
+	        alert.accept();
+		}
+
+
+}
