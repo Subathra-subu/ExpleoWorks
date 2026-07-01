7210df7f8c87a66c194a25cd865edc740ea087c2
Subathra-subu <subathrasubu2004@gmail.com>
2026-07-01 07:53:16 +0530

    Invalid test added

--- DemoBlaze_POM/tests/loginTest.test.ts
+++ DemoBlaze_POM/tests/loginTest.test.ts
@@ -8,15 +8,27 @@ const user:loginuser[] = readLoginData();
     test.beforeEach(async({homePage})=>{
         await homePage.navigate();
     })
+
     test('valid login',async({homePage,loginPage})=>{
         await homePage.clickLogin();
         await loginPage.login(loginData.validlogin.username,loginData.validlogin.password);
         await expect(homePage.successMessage).toHaveText(`Welcome ${loginData.validlogin.username}`); 
     });
 
-    test('invalid login',async({homePage,loginPage})=>{
+    test.only('invalid login',async({page,homePage,loginPage})=>{
+        
         await homePage.clickLogin();
-        await loginPage.login(user[0]!.username,user[0]!.password);
+
+        for(let i=0;i<user.length;i++){
+
+            const dialogPromise = page.waitForEvent('dialog');
+            await loginPage.login(user[i]!.username,user[i]!.password);
+
+            const dialog = await dialogPromise;
+            expect(dialog.message()).toBe(user[i]!.errormessage);
+            dialog.accept();
+        }
+
     });
 });
 
